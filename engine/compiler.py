ac649d70e8e287226da40453a0eeae3900f7a3c3
sh-navid <shamshirsaz.navid@live.com>
2024-03-26 11:08:37 +0330

    Moved read and write functions to File class

--- engine/compiler.py
+++ engine/compiler.py
@@ -9,6 +9,15 @@ class File:
                 return os.path.join(root, filename)
         return None
     
+    def read(path):
+        with open(path,"r") as f:
+            return f.read()
+        
+    def write(path,content):
+        with open(path,"w") as f:
+            f.write(content)
+
+    
 
     
 
--- html_terminal.py
+++ html_terminal.py
@@ -1,14 +1,8 @@
 import sys
 from engine.feature.html import PyHtml
+from engine.compiler import File
 
 
-def read(path):
-    with open(path,"r") as f:
-        return f.read()
-    
-def write(path,content):
-    with open(path,"w") as f:
-        f.write(content)
 
 
 if __name__=="__main__":
@@ -16,9 +10,9 @@ if __name__=="__main__":
     PATH_IN=ROOT+"/testcase/10/main.pyn"
     PATH_OUT=ROOT+"/testcase/10/main.pyn.out.py"
 
-    content = read(PATH_IN)
+    content = File.read(PATH_IN)
     if "pyn:compile" not in content:
         raise RuntimeError("There is no pyn:compile svsilsble in file")
 
     content=PyHtml().exec(content)
-    write(PATH_OUT,content)
+    File.write(PATH_OUT,content)
