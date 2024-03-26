ffc5cd181d3d12a3c5564036b70d755b561f7f56
sh-navid <shamshirsaz.navid@live.com>
2024-03-26 10:29:12 +0330

    Exten compiler to find files and dynamic imports

--- engine/compiler.py
+++ engine/compiler.py
@@ -1,7 +1,43 @@
-import sys
+import re
+import os
 
-if __name__=="__main__":
-    (compiler,file)=sys.argv
 
-    if compiler != "compiler.py":
-        raise RuntimeError("Use compiler.py directly")
+class File:
+    def find(filename, path='.'):
+        for root, dirs, files in os.walk(path):
+            if filename in files:
+                return os.path.join(root, filename)
+        return None
+    
+
+    
+
+
+class DynamicImport:
+    @staticmethod
+    def exec(content):
+        matches = re.findall(r"with +(.*) +import +(.*)", content)
+
+        for match in matches:
+            (module, imports) = match
+            print(module, imports)
+
+            result = File.find(f'{module}.py')
+            if not result:
+                ImportError("Cannot find module '{module}'")
+
+                
+
+
+    
+
+
+class Compiler:
+    @staticmethod
+    def compile_pyp(path):
+        print(path)
+
+        with open(path, "r") as f:
+            content = f.read()
+
+        DynamicImport.exec(content)
